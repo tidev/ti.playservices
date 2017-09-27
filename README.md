8a759cca050d89dd0e9c7346a32a75f95ac7144e
Gary Mathews <gmathews@appcelerator.com>
2017-09-27 16:21:03 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# ti.playservices
+
+Summary
+---------------
+The ti.playservices module is to be used with Titanium projects that depend on Google Play Services.
+
+Requirements
+---------------
+- Titanium Mobile SDK 6.3.0.GA or later
+
+Example
+---------------
+Please see the full-featured example in `android/example/app.js`.
+
+Build from Source
+---------------
+`appc run -p android --build-only` from the `android` directory
+
+Author
+---------------
+Axway
+
+License
+---------------
+Apache 2.0
+
+Contributing
+---------------
+Code contributions are greatly appreciated, please submit a new [pull request](https://github.com/appcelerator-modules/ti.playservices/pull/new/master)!
---
+++ android/src/ti/playservices/PlayServicesModule.java
@@ -0,0 +1,58 @@
+/**
+ * Axway Appcelerator Titanium - ti.playservices
+ * Copyright (c) 2017 by Axway. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+package ti.playervices;
+
+import org.appcelerator.kroll.annotations.Kroll;
+import org.appcelerator.kroll.KrollModule;
+import org.appcelerator.titanium.TiApplication;
+
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.ConnectionResult;
+
+@Kroll.module(name="PlayServices", id="ti.playervices")
+public class PlayServicesModule extends KrollModule
+{
+	private static final String TAG = "TiPlayServices";
+
+	private GoogleApiAvailability api;
+
+	@Kroll.constant public static final String GOOGLE_PLAY_SERVICES_PACKAGE = GoogleApiAvailability.GOOGLE_PLAY_SERVICES_PACKAGE;
+	@Kroll.constant public static final int GOOGLE_PLAY_SERVICES_VERSION_CODE = GoogleApiAvailability.GOOGLE_PLAY_SERVICES_VERSION_CODE;
+
+	@Kroll.constant public static final int RESULT_SUCCESS = ConnectionResult.SUCCESS;
+	@Kroll.constant public static final int RESULT_SERVICE_MISSING = ConnectionResult.SERVICE_MISSING;
+	@Kroll.constant public static final int RESULT_SERVICE_UPDATING = ConnectionResult.SERVICE_UPDATING;
+	@Kroll.constant public static final int RESULT_SERVICE_VERSION_UPDATE_REQUIRED = ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED;
+	@Kroll.constant public static final int RESULT_SERVICE_DISABLED = ConnectionResult.SERVICE_DISABLED;
+	@Kroll.constant public static final int RESULT_SERVICE_INVALID = ConnectionResult.SERVICE_INVALID;
+
+	public PlayServicesModule() {
+		super();
+
+		this.api = GoogleApiAvailability.getInstance();
+	}
+
+	@Kroll.method(runOnUiThread=true)
+	public void makeGooglePlayServicesAvailable() {
+		this.api.makeGooglePlayServicesAvailable(TiApplication.getAppRootOrCurrentActivity());
+	}
+
+	@Kroll.method
+	public int isGooglePlayServicesAvailable() {
+		return this.api.isGooglePlayServicesAvailable(TiApplication.getAppRootOrCurrentActivity());
+	}
+
+	@Kroll.method
+	public boolean isUserResolvableError(int code) {
+		return this.api.isUserResolvableError(code);
+	}
+
+	@Kroll.method
+	public String getErrorString(int code) {
+		return this.api.getErrorString(code);
+	}
+}
