1f91207a5f654de0310cbfe84a28f318974699d7
Chris Williams <chris.a.williams@gmail.com>
2019-08-30 15:02:32 -0400

    test: add hook to modify tiapp and add required metadata in android manifest

--- test/unit/karma.unit.config.js
+++ test/unit/karma.unit.config.js
@@ -1,15 +1,35 @@
 'use strict';
+const path = require('path');
+const fs = require('fs-extra');
+
+function projectManagerHook(projectManager) {
+	projectManager.once('prepared', function () {
+		const tiapp = path.join(this.karmaRunnerProjectPath, 'tiapp.xml');
+		const contents = fs.readFileSync(tiapp, 'utf8');
+
+		fs.writeFileSync(tiapp, contents.replace('</manifest>', `<application>
+		<meta-data
+            android:name="com.google.android.gms.ads.AD_MANAGER_APP"
+            android:value="true"/>
+	</application>
+</manifest>`), 'utf8');
+	});
+}
+projectManagerHook.$inject = [ 'projectManager' ];
 
 module.exports = config => {
 	config.set({
 		basePath: '../..',
-		frameworks: [ 'jasmine' ],
+		frameworks: [ 'jasmine', 'projectManagerHook' ],
 		files: [
 			'test/unit/specs/**/*spec.js'
 		],
 		reporters: [ 'mocha', 'junit' ],
 		plugins: [
-			'karma-*'
+			'karma-*',
+			 {
+				'framework:projectManagerHook': ['factory', projectManagerHook]
+			}
 		],
 		titanium: {
 			sdkVersion: config.sdkVersion || '8.1.1.GA'
