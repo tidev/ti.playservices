c1827bff7baff2a6e00af54af19bc5216a8546eb
Joshua Quick <jquick@axway.com>
2020-06-03 20:30:44 -0700

    feat: added "run-in-background" support
    - Titanium 9.1.0 supports running Titanium's JS runtime in the background without UI.
    - Requires change to bootstraps that show UI. They now must use async showUI() method instead of execute() method.

--- android/Resources/ti.playservices/ti.playservices.bootstrap.js
+++ android/Resources/ti.playservices/ti.playservices.bootstrap.js
@@ -1,27 +1,42 @@
 
-exports.execute = (finished) => {
-	// Check if Google Play Services validation is enabled on startup. (Enabled by default.)
-	// Can be disabled via "tiapp.xml" entry:
-	// <ti:app>
-	//   <property name="ti.playservices.validate.on.startup" type="bool">false</property>
-	// </ti:app>
-	const isEnabled = Ti.App.Properties.getBool('ti.playservices.validate.on.startup', true);
-	if (!isEnabled) {
-		finished();
-		return;
-	}
-
-	// Check if Google Play Services is available (ie: installed/updated) on devices that support it.
-	// If not available, will display Google's standard dialog asking end-user to install/update it.
+/**
+ * Checks if Google Play Services is available (ie: installed/updated) on devices that support it.
+ * If not available, will display Google's standard dialog asking end-user to install/update it.
+ * @param {Function} finished Callback to be invoked when this check has finished.
+ */
+function showUI(finished) {
 	try {
 		const PlayServices = require('ti.playservices');
+
+		// Check if Play Services needs to be installed, updated, or enabled.
+		let isUpdateNeeded = false;
+		const resultCode = PlayServices.isGooglePlayServicesAvailable();
+		if (resultCode === PlayServices.RESULT_SUCCESS) {
+			// Play Services is installed/updated.
+			const versionString = PlayServices.GOOGLE_PLAY_SERVICES_VERSION_CODE;
+			Ti.API.info(`ti.playservices: Google Play Services is available. (version: ${versionString})`);
+			isUpdateNeeded = false;
+		} else {
+			// Play Services needs to be installed, updated, or enabled.
+			// If invalid, then device does not support Google Play (such as Amazon devices) or it's a hacked version.
+			const errorString = PlayServices.getErrorString(resultCode);
+			Ti.API.info(`ti.playservices: Google Play Services is unavailable. (${errorString})`);
+			isUpdateNeeded = (resultCode !== PlayServices.RESULT_SERVICE_INVALID);
+		}
+
+		// Stop here if update is not required.
+		if (!isUpdateNeeded) {
+			finished();
+			return;
+		}
+
+		// Display Google's update dialog.
 		PlayServices.makeGooglePlayServicesAvailable((e) => {
 			if (e.success) {
-				// Play Services is installed/updated. Proceed to load "app.js".
+				// Play Services is installed/updated.
 				finished();
 			} else if (e.code === PlayServices.RESULT_SERVICE_INVALID) {
 				// Device does not support Google Play (such as an Amazon device) or it's a hacked version.
-				// Proceed to load "app.js". (It's impossible to install on device anyways.)
 				finished();
 			} else {
 				// Exit the app, because the end-user refused to install/update Play Services when prompted.
@@ -35,4 +50,13 @@ exports.execute = (finished) => {
 		Ti.API.error(err);
 		finished();
 	}
-};
+}
+
+// Only inject this bootstrap's UI on startup if Google Play Services validation is enabled.
+// Can be disabled via "tiapp.xml" entry:
+// <ti:app>
+//   <property name="ti.playservices.validate.on.startup" type="bool">false</property>
+// </ti:app>
+if (Ti.App.Properties.getBool('ti.playservices.validate.on.startup', true)) {
+	exports.showUI = showUI;
+}
